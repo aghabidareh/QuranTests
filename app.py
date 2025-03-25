9171bf64a100fa8681c74bb9fc16637d1b523141
aghabidareh <aghabidareh@gmail.com>
2025-03-25 00:01:13 -0700

    API

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask , request , jsonify
+from creator import create_question
+
+app = Flask(__name__)
+
+@app.route('/generate_questions', methods=['GET'])
+def generate_question():
+    try:
+        start = int(request.args.get('start'))
+        end = int(request.args.get('end'))
+        count = int(request.args.get('count'))
+
+    except:
+        return jsonify({'error': 'Invalid Input parameters.'})
+
+    question = create_question(start, end, count)
+
+    return jsonify({'question': question})
+
+if __name__ == '__main__':
+    app.run(debug=True)
